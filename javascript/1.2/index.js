2b3eb90d7f7e4ef4fe512cc9fa64fa9327807c5e
shadinab <shadinabwani2000@gmail.com>
2023-07-04 19:53:40 +0300

    1.2java

---
+++ javascript/1.2/index.js
@@ -0,0 +1,79 @@
+/*--Delete the wrong answers--
+1. Which of the following is/are strings?
+a) 4
+b) 4.0
+c) '4'
+
+-c-
+
+
+2. Which of the following is/are numbers?
+a) 4
+b) 4.0
+d) -4
+
+
+3. Which of the following is/are booleans?
+a) true
+b) false
+
+4. What is the result of 80 + 71.2?
+a) 151.2
+
+5. What is the result of "80" + 71.2?
+c) "8071.2"
+
+6. Does 100 + 30 produce a number or a string?
+a) number
+
+7. Does "100" + 30 produce a number or a string?
+b)string
+
+// */
+// //------Submit your answers under the
+// question------//
+// //create three different ways to declare variables
+let sun = "";
+var sun2 = "";
+const sun3 = ""
+// //declare a variable and reassign a value to it
+
+let sun = "50";
+sun =+ 10;
+// //create a variable and after that assign a string
+// with its value being: He's got it!
+// /*
+let vari = "null";
+let vari = "He's got it!"
+
+console.log(vari);
+
+// 1. create a variable and assign a value on how
+// much a restaurant bill is.
+
+let bill = null;
+bil = 200;
+
+// 2. create a variable and assign a value on how
+// much tax they should pay.
+let tax = null;
+tax = 100;
+// 3. create a variable that will calculate the bill
+// * tax and its output would be: Your total bill is
+// <total bill> $.
+// */
+let bill = 100; 
+let tax = 0.15; 
+
+let totalBill = bill *  tax;
+
+console.log("Your total bill is $" + totalBill);
+
+
+// // Round the number 50.6 to its nearest integer
+let roundNumber = Math.round (50.6);
+console.log(roundNumber)
+
+// //Create a variable that is undefined
+let myVariable;
+console.log(myVariable);
