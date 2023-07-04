8eba7331015a56dbc97036cc569e3e13bb8a2036
shadinab <shadinabwani2000@gmail.com>
2023-07-04 19:50:02 +0300

    1.1java

---
+++ javascript/1.1/index.js
@@ -0,0 +1,15 @@
+let message = "You enter a dimly lit room";
+let treasureCount = "15";
+let key = {goldenkey:"intricate carvings"};
+let isTrapActivated = true;
+let secretPassage = null;
+let mysteriousArtifact = undefined;
+let ancientSymbol = Symbol('stoneTable');
+
+console.log(typeof(message));
+console.log(typeof(treasureCount));
+console.log(typeof(key));
+console.log(typeof(isTrapActivated));
+console.log(typeof(secretPassage));
+console.log(typeof(mysteriousArtifact));
+console.log(typeof(ancientSymbol));
