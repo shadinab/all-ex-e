bc4a022426e593efd236027cc4ddcfc085c12d73
shadinab <shadinabwani2000@gmail.com>
2023-07-06 19:31:07 +0300

    2.1_java

---
+++ javascript/2.1/index.js
@@ -0,0 +1,32 @@
+// function countryInfo (country, population, capitalCity)
+// {
+
+// const country1 = [country];
+// const population1 = [population];
+// const capitalCity1 = [capitalCity];
+// return `${country1} has ${population1} people and its capital city is ${capitalCity1}`;
+// }
+
+
+// const value = countryInfo('japan','10million','tokyo')
+// console.log(value);
+
+
+function countryInfo (country, population, capitalCity)
+{
+
+const country1 = [country];
+const population1 = [population];
+const capitalCity1 = [capitalCity];
+return `${country1} has ${population1} people and its capital city is ${capitalCity1}`;
+}
+
+
+const value1 = countryInfo('japan','10million','tokyo'); 
+console.log(value1);
+
+const value2 = countryInfo('israel','7million','jeruslem'); 
+console.log(value2);
+
+const value3= countryInfo('egypt','100million','cairo'); 
+console.log(value3);
